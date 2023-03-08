0be3104e05bd3edc9a45d0ed37f485164b8c8ff8
Camilo Garcia <garc0236@algonquinlive.com>
2023-03-07 22:33:25 -0500

    resolves problems with js, html, and css

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
-const $cwrapper = document.getElementById('cwrapper')
+const $cwrapper = document.querySelector('.cwrapper')
 
 const emojis = []
 
 for (let i = 127801; i < 127850; i++) {
     emojis.push(`
-    <div class="emoji">&#${i};</div>`)
+    <div class="emoji">&#${i};<p class="ref">${i}</p></div>
+    `)
 }
 
 $cwrapper.innerHTML +=  emojis.join('')
